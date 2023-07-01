9edf0247ce323bea9d328fca85177a2c0d1f871b
OlegBarabash <olegbarabash7@gmail.com>
2023-07-01 22:52:45 +0300

    Done)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,29 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const KEY = 'videoplayer-current-time'
+const startPlayTime = localStorage.getItem(KEY) || 0.00
 
-player.on('timeupdate', onPlay);
+player.setCurrentTime(startPlayTime).catch(handlerError);
+player.on('timeupdate', throttle(onPlay, 1000) );
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+function onPlay() {
+    player.getCurrentTime().then( seconds => 
+        localStorage.setItem(KEY, seconds)
+    ) 
+}
 
-function onPlay(data) {
-    console.log("timeupdate: ", data);
+function handlerError(error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log('the time was less than 0 or greater than the videos duration');
+            player.setCurrentTime(0.00)
+            break;
+        default:
+            console.log('some other error occurred');
+            player.setCurrentTime(0.00)
+            break;
+    }
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form')
+const emailForm = document.querySelector('INPUT')
+const messageForm = document.querySelector('TEXTAREA')
+const KEY = "feedback-form-state"
+const msgObj = {
+    email: '',
+    message: '',
+}
+
+form.addEventListener("input", throttle(handlerFormInput, 500))
+form.addEventListener("submit", handlerFormSubmit)
+
+isContinue(msgObj)
+
+function isContinue({email,message}) {
+    if (!localStorage.length) {
+        return
+    }
+    const storage = JSON.parse(localStorage.getItem(KEY)) 
+    email = storage.email
+    message = storage.message
+
+    emailForm.value = email
+    messageForm.value = message
+}
+
+function handlerFormInput(eve) {
+    const msg = eve.target.value
+    if (eve.target.name === 'email') {
+        msgObj.email = msg
+    }else if (eve.target.name === 'message') {
+        msgObj.message = msg
+    }
+    localStorage.setItem(KEY, JSON.stringify(msgObj))
+}
+
+function handlerFormSubmit(eve) {
+    eve.preventDefault()
+    console.log(msgObj);
+    eve.currentTarget.reset()
+    localStorage.removeItem(KEY)
+}
